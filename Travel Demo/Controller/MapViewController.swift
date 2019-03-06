594fa7a8c592b4e7ed33ec16fb371e14e1ee7c4e
Sean <seangroebe@gmail.com>
2019-03-06 12:59:12 -0600

    Got mapView discover button set. Merging back into master

--- Travel Demo/Controller/MapViewController.swift
+++ Travel Demo/Controller/MapViewController.swift
@@ -56,6 +56,7 @@ class MapViewController: UIViewController {
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
 		navigationController?.setNavigationBarHidden(true, animated: animated)
+		buttonBackgroundView.layer.cornerRadius = 10
 	}
 	
 	override func viewWillDisappear(_ animated: Bool) {
