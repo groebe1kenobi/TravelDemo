a14c71317efbcce5593977e433dad34cb7fa321b
Sean <seangroebe@gmail.com>
2019-03-23 18:44:09 -0500

    Updated Container view to support segmented switching from multiple views inside subview

--- Travel Demo/Controller/ViewControllers/UserProfileViewController.swift
+++ Travel Demo/Controller/ViewControllers/UserProfileViewController.swift
@@ -13,24 +13,33 @@ class UserProfileViewController: UIViewController {
 	
 
 	@IBOutlet weak var profilePicImageView: UIImageView!
+	@IBOutlet weak var segControl: UISegmentedControl!
+	@IBOutlet weak var containerView: UIView!
+	
+	
+	private lazy var toVisitViewController: ToVisitTableViewController = {
+		let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+		let viewController = storyboard.instantiateViewController(withIdentifier: "ToVisitTableView") as! ToVisitTableViewController
+		self.add(asChildViewController: viewController)
+		return viewController
+	}()
+	
+	private lazy var secondViewController: SecondViewController = {
+		let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+		let viewController = storyboard.instantiateViewController(withIdentifier: "SecondVC") as! SecondViewController
+		self.add(asChildViewController: viewController)
+		return viewController
+	}()
 	
 	var myLandmarks = [Landmark]()
 	
-	private var landmarkTableViewController: ToVisitTableViewController?
+	//private var landmarkTableViewController: ToVisitTableViewController?
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
-		
-		guard let landmarkController = children.first as? ToVisitTableViewController else {
-			fatalError()
-		}
-		landmarkTableViewController = landmarkController
-		//landmarkController.delegate = self
-		navigationController?.title = currentUser.firstName
 		profilePicImageView.image = currentUser.proPic
 		
-		
+		setupView()
 		
 		
 		
@@ -49,12 +58,50 @@ class UserProfileViewController: UIViewController {
 		//defaults.getUserDefaults()
 	}
 	
+	private func setupView() {
+		setupSegmentedControl()
+		updateView()
+	}
 	
+	private func setupSegmentedControl() {
+		segControl.removeAllSegments()
+		segControl.insertSegment(withTitle: "ToVisit", at: 0, animated: false)
+		segControl.insertSegment(withTitle: "Photos", at: 1, animated: false)
+		segControl.addTarget(self, action: #selector(selectionDidChange(_:)), for: .valueChanged)
+		
+		segControl.selectedSegmentIndex = 0
+	}
 	
+	@objc func selectionDidChange(_ sender: UISegmentedControl) {
+		updateView()
+	}
 	
+	func updateView() {
+		if segControl.selectedSegmentIndex == 0 {
+			remove(asChildViewController: secondViewController)
+			add(asChildViewController: toVisitViewController)
+		} else {
+			remove(asChildViewController: toVisitViewController)
+			add(asChildViewController: secondViewController)
+		}
+	}
 	
+	private func add(asChildViewController viewController: UIViewController) {
+		addChild(viewController) // add child vc
+		containerView.addSubview(viewController.view) // add child vc as subview
+		
+		// Configure child view
+		viewController.view.frame = containerView.bounds
+		viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+		
+		// notify child view controller
+		viewController.didMove(toParent: self)
+	}
 	
+	private func remove(asChildViewController viewController: UIViewController) {
+		viewController.willMove(toParent: nil) 	// Notify Child VC
 
-
-
+		viewController.view.removeFromSuperview() // Remove child view from superview
+		viewController.removeFromParent() // notify child vc
+	}
 }
